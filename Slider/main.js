84d16fc41ed0d22beae5dafaa4c5ffae649bc405
adamfed7 <91409021+adamfed7@users.noreply.github.com>
2023-01-08 23:17:54 +0100

    Finish slider project
    Zrobienie animacji przewijania i przenikania, podpięcie przycisku do zmiany animacji, drobne poprawki, uproszczenie kodu

--- Slider/main.js
+++ Slider/main.js
@@ -1,106 +1,201 @@
 var slideIndex = 1;
-var paused = false;
-var slideFade = false;
+var paused = true;
+var slideFade = true;
 var intervalId = null;
+var tranformSlide;
+var tranformSlideOpa;
 
 function showSlide(n) {
-  var i;
   var slides = document.getElementsByClassName("slide");
   var dots = document.getElementsByClassName("dot");
-
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-
+  var i;
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].classList.remove('prevSlide');
     slides[i].classList.remove('active-slide');
+    slides[i].classList.remove('nextSlide');
+
+    dots[i].classList.remove('prevDot');
+    dots[i].classList.remove('active-dot');
+    dots[i].classList.remove('nextDot');
   }
 
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  x = n - 1;
+  if (x < 0) {
+    x = 5;
   }
-  slides[slideIndex - 1].style.display = "block";
-  slides[slideIndex - 1].classList.add('active-slide');
-  dots[slideIndex - 1].className += " active";
+
+  y = n + 1;
+  if (y > slides.length - 1) {
+    y = 0;
+  }
+
+  slides[x].classList.add('prevSlide');
+  slides[n].classList.add('active-slide');
+  slides[y].classList.add('nextSlide');
+
+  dots[x].classList.add('prevDot');
+  dots[n].classList.add('active-dot');
+  dots[y].classList.add('nextDot');
 }
 
 function currentSlideShow(n) {
   showSlide(slideIndex = n);
 }
 
-function nextSlide() {
+function moveSlide(x) {
   var currentSlide = document.querySelector('.active-slide');
-  var currentDot = document.querySelector('.active');
-  var slides = document.querySelectorAll('.slide');
-  var nextSlide = currentSlide.nextElementSibling;
-  var nextDot = currentDot.nextElementSibling;
-  
-  if (nextDot == null) {
-    nextDot = document.querySelector('.firstDot');
-  }
+  var prevSlide = document.querySelector('.prevSlide');
+  var nextSlide = document.querySelector('.nextSlide');
 
-  currentDot.classList.remove('active');
-  nextDot.classList.add('active');
+  var prevDot = document.querySelector('.prevDot')
+  var currentDot = document.querySelector('.active-dot');
+  var nextDot = document.querySelector('.nextDot')
 
-  if (nextSlide == null) {
-    nextSlide = document.querySelector('.first');
-  }
-
-  currentSlide.style.display = "none";
-  setSlideAnimation('cubic-bezier(0.25, 0.1, 0.25, 1)');
-  nextSlide.style.display = "block";
-
+  prevSlide.classList.remove('prevSlide');
   currentSlide.classList.remove('active-slide');
-  nextSlide.classList.add('active-slide');
-  
+  nextSlide.classList.remove('nextSlide');
+
+  prevDot.classList.remove('prevDot');
+  currentDot.classList.remove('active-dot');
+  nextDot.classList.remove('nextDot');
+
+  if (x === 1) {
+    prevSlide = currentSlide;
+    currentSlide = nextSlide;
+    nextSlide = nextSlide.nextElementSibling;
+
+    if (nextSlide == null) {
+      nextSlide = document.querySelector('.firstSlide');
+    }
+
+    prevDot = currentDot;
+    currentDot = nextDot;
+    nextDot = nextDot.nextElementSibling;
+
+    if (nextDot == null) {
+      nextDot = document.querySelector('.firstDot');
+    }
+
+    if(slideFade){
+      prevSlideMoveSlide(prevSlide, 1);
+      nextSlideMoveSlide(currentSlide, 1);
+    }else{
+      slideMoveFade(currentSlide);
+    }
+
+  } else if (x === -1) {
+    nextSlide = currentSlide;
+    currentSlide = prevSlide;
+    prevSlide = prevSlide.previousElementSibling;
+    if (prevSlide == null) {
+      prevSlide = document.querySelector('.lastSlide');
+    }
+
+    nextDot = currentDot;
+    currentDot = prevDot;
+    prevDot = prevDot.previousElementSibling;
+    if (prevDot == null) {
+      prevDot = document.querySelector('.lastDot');
+    }
+
+    if(slideFade){
+      prevSlideMoveSlide(currentSlide, -1);
+      nextSlideMoveSlide(nextSlide, -1);
+    }else{
+      slideMoveFade(currentSlide);
+    }
+
+  }
+
+  prevSlide.classList.add('prevSlide');
+  currentSlide.classList.add('active-slide');
+  nextSlide.classList.add('nextSlide');
+
+  prevDot.classList.add('prevDot');
+  currentDot.classList.add('active-dot');
+  nextDot.classList.add('nextDot');
 }
 
-function backSlide() {
-  var currentSlide = document.querySelector('.active-slide');
-  var currentDot = document.querySelector('.active');
-  var slides = document.querySelectorAll('.slide');
-  var backSlide = currentSlide.previousElementSibling;
-  var backDot = currentDot.previousElementSibling;
+function nextSlideMoveSlide(nextSlide, prevNext) {
+  const a = 'translate(50%,-50%) scale(0.5)';
+  const b = 'translate(-50%,-50%) scale(1)';
 
-  if (backDot == null) {
-    backDot = document.querySelector('.lastDot');
+  if (prevNext === 1) {
+    tranformSlide = [
+      {transform: a},
+      {transform: b}
+    ];
+  } else if (prevNext === -1) {
+    tranformSlide = [
+      {transform: b},
+      {transform: a}
+    ];
   }
 
-  currentDot.classList.remove('active');
-  backDot.classList.add('active');
+  const timing = {
+    duration: 1000,
+    iterations: 1,
+  }
+  const slide = nextSlide;
+  slide.animate(tranformSlide, timing);
+}
 
-  if (backSlide == null) {
-    backSlide = document.querySelector('.last');
+function prevSlideMoveSlide(prevSlide, prevNext) {
+  const a = 'translate(-50%,-50%) scale(1)';
+  const b = 'translate(-150%,-50%) scale(0.5)';
+
+  if (prevNext === 1) {
+    tranformSlide = [
+      {transform: a},
+      {transform: b}
+    ];
+  } else if (prevNext === -1) {
+    tranformSlide = [
+      {transform: b},
+      {transform: a}
+    ];
   }
 
-  backSlide.style.display = "block";
-  currentSlide.style.display = "none";
-  backSlide.classList.add('active-slide');
-  currentSlide.classList.remove('active-slide');
+  const timing = {
+    duration: 1000,
+    iterations: 1,
+  }
 
+  const slide = prevSlide;
+  slide.animate(tranformSlide, timing);
+
+}
+
+function slideMoveFade(currentSlide){
+  tranformSlideOpa = [
+    {opacity: '0.2'},
+    {opacity: '1'}
+  ];
+
+  const timing = {
+    duration: 1000,
+    iterations: 1,
+  }
+  const slide = currentSlide;
+  slide.animate(tranformSlideOpa, timing);
 }
 
 function pauseSlideshow() {
   var pauseButton = document.getElementById("pause");
-
   if (paused) {
     paused = false;
-    pauseButton.innerHTML = "Play";
-    stopSlider();
+    pauseButton.innerHTML = "Pause";
+    startSlider();
 
   } else {
     paused = true;
-    pauseButton.innerHTML = "Pause";
-    startSlider();
+    pauseButton.innerHTML = "Play";
+    stopSlider();
   }
 }
 
 function startSlider() {
-  intervalId = setInterval(nextSlide, 1000);
+  intervalId = setInterval(moveSlide, 2000, 1);
 }
 
 function stopSlider() {
@@ -109,25 +204,13 @@ function stopSlider() {
 }
 
 function changeAnimation() {
-  var slideFadeButton = document.getElementById("slideFade");
-
+  var pauseButton = document.getElementById("slideFade");
   if (slideFade) {
     slideFade = false;
-    slideFadeButton.innerHTML = "Slide";
-    
+    pauseButton.innerHTML = "Fade";
 
   } else {
     slideFade = true;
-    slideFadeButton.innerHTML = "Fade";
+    pauseButton.innerHTML = "Slide";
   }
 }
-
-function setSlideAnimation(type) {
-  const slides = document.querySelectorAll('.slide');
-
-  slides.forEach((slide) => {
-    slide.style.transition = `all ${type}`;
-  });
-}
-
-
