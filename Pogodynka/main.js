c0c3164475ae8a54ecfefbdcbac257bc841699b7
adamfed7 <91409021+adamfed7@users.noreply.github.com>
2023-01-22 23:44:32 +0100

    Create new schema for Pogodynka and correct JS code

--- Pogodynka/main.js
+++ Pogodynka/main.js
@@ -1,71 +1,55 @@
-let city = "Podwilk";
-const searchForm = document.querySelector("#search"),
-    resultCard = document.querySelector('#result'),
-    cityOut_elem = document.querySelector('#cityOut'),
-    mainIcon_elem = document.querySelector('#mainIcon'),
-    mainTemp_elem = document.querySelector('#mainTemp'),
-    humidity_elem = document.querySelector('#humidity'),
-    submitButton = document.querySelector("#searchButton"),
-    inputCityName = document.querySelector('#cityIn');
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".weather .cities");
 
-document.addEventListener("DOMContentLoaded", function () {
-   loadWeatherData(function () {
-        resultCard.style.display = "block";
+const apiKey = "5935862069e9fbfb26646d363d2ef7bd";
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  let inputVal = input.value;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const {
+        main,
+        name,
+        sys,
+        weather
+      } = data;
+      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+        weather[0]["icon"]
+      }.svg`;
+
+      const li = document.createElement("li");
+      li.classList.add("city");
+      const markup = `
+        <h2 class="city-name" data-name="${name},${sys.country}">
+          <span>${name}</span>
+          <sup>${sys.country}</sup>
+        </h2>
+        </br>
+        <h3>Temperatura:</h3>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup>
+        <figure>
+          <img class="city-icon" src="${icon}" alt="${weather[0]["description"]}">
+        </figure>
+        </div>
+        
+        <h3>Wilgotność: </h3>
+        <div class="city-hum">${main.humidity}<sup>%</sup></div>
+      `;
+      li.innerHTML = markup;
+      list.appendChild(li);
+    })
+    .catch(() => {
+      msg.textContent = "Podaj właściwą nazwę";
     });
+
+  msg.textContent = "";
+  form.reset();
+  input.focus();
 });
-
-searchForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-   if (this.checkValidity()) {
-        submitButton.disabled = true;
-        submitButton.value = "Loading...";
-        city = inputCityName.value;
-        loadWeatherData(function () {
-            inputCityName.value = null;
-        });
-    }
-});
-
-function loadWeatherData(callbackFunction) {
-
-    const request = new XMLHttpRequest();
-    const key = "eb9ba4d5906d3eecd90f0bb03297ec8b";
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric" +
-        "&APPID=" + key;
-
-    request.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            const response = JSON.parse(this.responseText);
-            updateWeatherData(response, callbackFunction);
-        }
-    };
-
-    request.open("GET", url, true);
-    request.send();
-}
-
-function updateWeatherData(data, callbackFunction = null) {
-    const cityName = data.name,
-        icon = data.weather[0].icon,
-        icon_url = "http://openweathermap.org/img/wn/" + icon + "@2x.png",
-        description = data.weather[0].description,
-        temp = data.main.temp,
-        humidity = data.main.humidity,
-        deg = "°",
-        percent = "%";
-
-   cityOut_elem.innerHTML = cityName;
-    mainIcon_elem.setAttribute("src", icon_url);
-    mainIcon_elem.setAttribute("alt", description);
-    mainTemp_elem.innerHTML = Math.round(temp) + deg;
-    humidity_elem.innerHTML = humidity + percent;
-
-    submitButton.disabled = false;
-    submitButton.value = "Get Weather";
-
-  if (callbackFunction) {
-      callbackFunction();
-    }
-}
