641b88bb30b14cf772a26f17b4ef7d7930699074
adamfed7 <91409021+adamfed7@users.noreply.github.com>
2023-01-31 13:23:07 +0100

    Make simple notekeep template

--- Notekeep/main.js
+++ Notekeep/main.js
@@ -1,3 +1,14 @@
+function ShowAddNote(){
+    document.querySelector('.Notes').style.display = 'block';
+}
 
+
+class Note {
+    constructor(title, describe, noteColor) {
+      this.title = title;
+      this.describe = describe;
+      this.noteColor = noteColor;
+    }
+}
 
 
